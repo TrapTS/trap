d4ce803296279d09e7ce6299a3383871bccfaead
AlfieriChou <alfierichou@gmail.com>
2018-12-26 16:31:57 +0800

    base service add knex to this

---
+++ src/app/common/base_service.ts
@@ -0,0 +1,23 @@
+import { config } from '../../config'
+import * as knex from 'knex'
+import { KnexConfig } from '../../type/knex'
+
+const knexConfig: KnexConfig = {
+  client: 'mysql',
+  connection: {
+    host: config['mysql'].host,
+    user: config['mysql'].user,
+    password: config['mysql'].password,
+    database: config['mysql'].database,
+    supportBigNumbers: true,
+    charset: 'utf8mb4',
+    connectTimeout: 15000
+  }
+}
+
+class BaseService {
+  knex: any
+  constructor () {
+    this.knex = knex(knexConfig)
+  }
+}
--- src/config/default.config.ts
+++ src/config/default.config.ts
@@ -1,6 +1,9 @@
 export const config: Object = {
   mysql: {
-
+    host: '106.15.230.136',
+    user: 'lvyang',
+    password: 'zhazhayang',
+    database: 'test'
   },
   port: 4000
 }

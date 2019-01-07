6b770a5b6bd8d875e0e87df6f44b904b38ea158c
AlfieriChou <alfierichou@gmail.com>
2019-01-07 17:41:20 +0800

    add receiver rabbit mq

---
+++ src/rabbitmq/test_re.ts
@@ -0,0 +1,31 @@
+const amqp = require('amqplib')
+
+const queue: string = 'Hello'
+
+async function helloWorld() {
+  const connection = await amqp.connect('amqp://47.74.234.5:5672')
+  console.info('connect to RabbitMQ success')
+  try {
+    const channel = await connection.createChannel()
+    await channel.assertQueue(queue)
+    await channel.consume(queue, async (message) => {
+      console.log(message.content.toString())
+      channel.ack(message)
+    })
+
+    connection.on('error', (err) => {
+      console.log(err)
+      setTimeout(helloWorld, 10000)
+    })
+
+    connection.on('close', () => {
+      console.error('connection to RabbitQM closed!')
+      setTimeout(helloWorld, 10000)
+    })
+  } catch (err) {
+    console.error(err)
+    setTimeout(helloWorld, 10000)
+  }
+}
+
+helloWorld()
--- src/rabbitmq/test_se.ts
+++ src/rabbitmq/test_se.ts
@@ -1,27 +1,27 @@
 import * as amqp from 'amqplib'
 import { config } from '../config'
 
-const queue = 'Hello'
+const queue: string = 'Hello'
 
 const helloWorld: Function = async () => {
   const connection: any = await amqp.connect(config.amqp_url)
-  console.info('------->', 'RabbitMQ success!!!')
+  console.info('RabbitMQ success!!!')
   try {
     const channel: any = await connection.createChannel()
     await channel.assertQueue(queue)
-    await channel.sendToQueue(queue, new Buffer('Hello World'), {
+    await channel.sendToQueue(queue, Buffer.from('Hello World'), {
       persistent: true
     })
     connection.on('error', err => {
-      console.log('------>', err)
+      console.log(err)
       setTimeout(helloWorld(), 1000)
     })
     connection.on('close', () => {
-      console.log('------->', 'RabbitQM closed!!!')
+      console.log('RabbitQM closed!!!')
       setTimeout(helloWorld(), 1000)
     })
   } catch (err) {
-    console.error('------->', err)
+    console.error(err)
     setTimeout(helloWorld(), 1000)
   }
 }

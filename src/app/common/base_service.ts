9a2c4b8579967b8433deadbafc4b3479228d8754
AlfieriChou <alfierichou@gmail.com>
2019-01-17 08:52:46 +0800

    prettier format and update package.json

--- src/app/common/base_service.ts
+++ src/app/common/base_service.ts
@@ -1,6 +1,6 @@
-import {config } from '../../config'
+import { config } from '../../config'
 import * as Knex from 'knex'
-import knex from '../../database';
+import knex from '../../database'
 export class BaseService {
   knex: Knex
   config: Object

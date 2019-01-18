b526faba0e720a4415e067b9242034ba55f472f4
AlfieriChou <alfierichou@gmail.com>
2019-01-18 12:48:38 +0800

    Mr.Long say the right

--- src/typings/app/decorator/index.d.ts
+++ src/typings/app/decorator/index.d.ts
@@ -1,2 +1,7 @@
-export type Prefix = (path: string) => any
-export type Route = (path: string, method: any) => any
+export type Prefix = (
+  path: string
+) => (target: any, _key?: string | symbol, _descriptor?: any) => void
+export type Route = (
+  path: string,
+  method: any
+) => (target: any, _key?: string | symbol, descriptor?: any) => void

a7649b2ba5f78d83595a8d73c5c0e07a299412eb
AlfieriChou <alfierichou@gmail.com>
2019-01-19 11:47:31 +0800

    typings all export

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -2,6 +2,29 @@ import * as Koa from 'koa'
 import { IService } from './app/service'
 import { IModel } from './app/model'
 import { Config } from './config'
+import { TypeList, Socket } from './websocket'
+import {
+  CronSchedule,
+  Schedule,
+  ClassSchedule,
+  EntitySchedule,
+  EntitySubcription
+} from './schedule'
+import { ReceiveRabbitMQ, SendMessageFunc } from './rabbitmq'
+import {
+  Migration,
+  CreateTable,
+  Drop,
+  Raw,
+  RenameTable,
+  RenameColumn,
+  DropColumn,
+  AddColumn
+} from './migrations'
+import { Helper } from './extends'
+import { Mysql } from './database'
+import { EntityService } from './app/common'
+import { Prefix, Route } from './app/decorator'
 
 declare module 'koa' {
   interface Request extends Koa.BaseRequest {
@@ -24,4 +47,32 @@ export class InitServer {
 
 export class InitWSServer {
   public init(): void
+}
+
+export {
+  TypeList,
+  Socket,
+  CronSchedule,
+  Schedule,
+  ClassSchedule,
+  EntitySchedule,
+  EntitySubcription,
+  ReceiveRabbitMQ,
+  SendMessageFunc,
+  Migration,
+  CreateTable,
+  AddColumn,
+  DropColumn,
+  RenameColumn,
+  RenameTable,
+  Raw,
+  Drop,
+  Helper,
+  Mysql,
+  Config,
+  EntityService,
+  Prefix,
+  Route,
+  IModel,
+  IService
 }

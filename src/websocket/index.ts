33e4596ed499207903873d244d8166ad6ddf033d
AlfieriChou <alfierichou@gmail.com>
2019-01-23 11:26:06 +0800

    websocket

--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -1,7 +1,7 @@
 import * as socket from 'socket.io'
 import { config } from '../config'
 import * as dir from 'dir_filenames'
-import * as _ from 'lodash'
+import { isPlainObject } from 'lodash'
 
 class Server {
   io: any
@@ -21,34 +21,34 @@ class Server {
 
         const files: string[] = dir(`${config.appRoot}/src/websocket/normal`)
         files.map(file => {
-          const socketObj = require(file)
+          const socketObj: Object = require(file)
           for (let i in socketObj) {
-            if (!_.isPlainObject(socketObj[i]))
-              throw new Error('Socket configure mast be Object!!!')
-            if (!socketObj[i].type || !socketObj[i].options)
-              throw new Error('Socket module mast have type and options!!!')
-            if (socketObj[i].type === 'ON' && socketObj[i].channel) {
-              console.info('type: on, start at: ' + Date.now())
-              socket.on(socketObj[i].channel, socketObj[i].options)
-            }
-            if (socketObj[i].type === 'EMIT' && socketObj[i].channel) {
-              console.info('type: emit, start at: ' + Date.now())
-              socket.emit(socketObj[i].channel, socketObj[i].options)
-            }
-            if (socketObj[i].type === 'RAW') {
-              console.info('type: raw, start at: ' + Date.now())
-              const rawFunc: Function = socketObj[i].options
-              rawFunc(socket)
-            }
-            if (socketObj[i].logger) {
-              console.info(
-                JSON.stringify({
-                  wsEngine: 'ws',
-                  header: JSON.stringify(socket.conn.request.headers),
-                  url: socket.conn.request.url,
-                  method: socket.conn.request.method
-                })
-              )
+            if (isPlainObject(socketObj[i])) {
+              if (!socketObj[i].type || !socketObj[i].options)
+                throw new Error('Socket module mast have type and options!!!')
+              if (socketObj[i].type === 'ON' && socketObj[i].channel) {
+                console.info('type: on, start at: ' + Date.now())
+                socket.on(socketObj[i].channel, socketObj[i].options)
+              }
+              if (socketObj[i].type === 'EMIT' && socketObj[i].channel) {
+                console.info('type: emit, start at: ' + Date.now())
+                socket.emit(socketObj[i].channel, socketObj[i].options)
+              }
+              if (socketObj[i].type === 'RAW') {
+                console.info('type: raw, start at: ' + Date.now())
+                const rawFunc: Function = socketObj[i].options
+                rawFunc(socket)
+              }
+              if (socketObj[i].logger) {
+                console.info(
+                  JSON.stringify({
+                    wsEngine: 'ws',
+                    header: JSON.stringify(socket.conn.request.headers),
+                    url: socket.conn.request.url,
+                    method: socket.conn.request.method
+                  })
+                )
+              }
             }
           }
         })

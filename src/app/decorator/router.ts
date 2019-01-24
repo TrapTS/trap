88d13f0eec28da899ad874785ca04edb8b840812
AlfieriChou <alfierichou@gmail.com>
2019-01-24 17:54:19 +0800

    rewrite route decorator

--- src/app/decorator/router.ts
+++ src/app/decorator/router.ts
@@ -19,47 +19,55 @@ export enum Method {
 
 export const prefix: Prefix = (path: string = '') => {
   return (target: any, _key?: string | symbol, _descriptor?: any): void => {
-    if (!target.prototype.router) {
-      target.prototype.router = new Router()
-    }
-    router.use(path, target.prototype.router.routes())
-    router.use(path, target.prototype.router.allowedMethods())
+    prefixUpdateRoute(target, path)
   }
 }
 
 export const route: Route = (path: string, method: Method) => {
   return (target: any, _key?: string | symbol, descriptor?: any): void => {
-    if (!target.router) {
-      target.router = new Router()
-    }
-    switch (method) {
-      case Method.HEAD:
-        target.router.head(path, descriptor.value)
-        break
-      case Method.OPTIONS:
-        target.router.options(path, descriptor.value)
-        break
-      case Method.GET:
-        target.router.get(path, descriptor.value)
-        break
-      case Method.PATCH:
-        target.router.patch(path, descriptor.value)
-        break
-      case Method.PUT:
-        target.router.put(path, descriptor.value)
-        break
-      case Method.POST:
-        target.router.post(path, descriptor.value)
-        break
-      case Method.DELETE:
-        target.router.del(path, descriptor.value)
-        break
-      case Method.ALL:
-        target.router.all(path, descriptor.value)
-        break
-      default:
-        throw new Error('@route decorator "method" is not valid')
-    }
+    routeUpdate(target, method, path, descriptor)
+  }
+}
+
+const prefixUpdateRoute = (target: any, path: string): void => {
+  if (!target.prototype.router) {
+    target.prototype.router = new Router()
+  }
+  router.use(path, target.prototype.router.routes())
+  router.use(path, target.prototype.router.allowedMethods())
+}
+
+const routeUpdate = (target: any, method: Method, path: string, descriptor?: any) => {
+  if (!target.router) {
+    target.router = new Router()
+  }
+  switch (method) {
+    case Method.HEAD:
+      target.router.head(path, descriptor.value)
+      break
+    case Method.OPTIONS:
+      target.router.options(path, descriptor.value)
+      break
+    case Method.GET:
+      target.router.get(path, descriptor.value)
+      break
+    case Method.PATCH:
+      target.router.patch(path, descriptor.value)
+      break
+    case Method.PUT:
+      target.router.put(path, descriptor.value)
+      break
+    case Method.POST:
+      target.router.post(path, descriptor.value)
+      break
+    case Method.DELETE:
+      target.router.del(path, descriptor.value)
+      break
+    case Method.ALL:
+      target.router.all(path, descriptor.value)
+      break
+    default:
+      throw new Error('@route decorator "method" is not valid')
   }
 }
 
--- src/rabbitmq/receive/test.ts
+++ src/rabbitmq/receive/test.ts
@@ -1,8 +1,10 @@
 import { ReceiveRabbitMQ } from '../../typings'
+import { ConsumeMessage } from 'amqplib'
 
 export const test: ReceiveRabbitMQ = {
   chananel: 'Hello',
-  task: message => {
+  task: (message: ConsumeMessage | null) => {
+    if (!message) throw new Error('message is null!!')
     console.log(message.content.toString())
   }
 }

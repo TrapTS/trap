8684df0cb4b51d0eb27b3ef63d7f2dfdd6ee1843
AlfieriChou <alfierichou@gmail.com>
2019-02-01 20:13:40 +0800

    rewrite migration drop table

--- src/migrations/drop.ts
+++ src/migrations/drop.ts
@@ -1,17 +1,17 @@
 import { readdirSync } from 'fs'
 import { join } from 'path'
-import { isPlainObject, union } from 'lodash'
+import { union } from 'lodash'
 import { db } from './index'
 import { Migration } from './types'
 import { Operation } from './enum'
 
 let tasks: Function[] = []
 readdirSync(join(__dirname, './operation')).map(file => {
-  let migrations = require(join(__dirname, './operation/' + file))(db)
+  let migrations = require(join(__dirname, './operation/' + file))
   let funcArray: Function[] = []
   for (let i in migrations) {
     const migration: Migration = migrations[i]
-    if (isPlainObject(migration) && migration.opt === Operation.drop) {
+    if (migration.opt === Operation.drop) {
       funcArray.push(() => {
         return db.schema.dropTable(<string>migration.table)
       })
@@ -22,9 +22,9 @@ readdirSync(join(__dirname, './operation')).map(file => {
 
 const schedule = async () => {
   for await (let task of tasks) {
-    console.log('sync db done!')
     await task()
   }
+  console.log('sync db done!')
   process.exit()
 }
 
